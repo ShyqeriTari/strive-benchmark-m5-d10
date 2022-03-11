8431ab2131a61c32fdaa3917d74d3075aa468a17
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-11 12:31:18 +0100

    Post method

--- src/services/media/index.js
+++ src/services/media/index.js
@@ -4,8 +4,8 @@ import { fileURLToPath } from "url"
 import fs from "fs"
 import uniqid from "uniqid"
 import { newMediaValidation } from "./mediaValidation.js"
-
-
+import createHttpError from "http-errors"
+import { validationResult } from "express-validator"
 
 const mediaJSONpath = join(dirname(fileURLToPath(import.meta.url)), "media.json")
 
@@ -18,25 +18,23 @@ const mediaRouter = express.Router()
 mediaRouter.post("/", newMediaValidation, (req, res, next) => {
     try {
         const mediaArray = getMedia()
-        const errorGroup = validationResult(request)
-        if (errorGroup.isEmpty()) {
-
-            const newMedia = { imdbID: uniqid(), ...req.body }
+        const errorGroup = validationResult(req)
+        if(errorGroup.isEmpty()){
+            const newMedia = {imdbID: uniqid(), ...req.body  }
 
             mediaArray.push(newMedia)
 
             writeMedia(mediaArray)
 
-            res.status(201).send({ newMedia })
-
+            res.status(201).send({newMedia})
         } else {
-            next(createHttpError(400, "Some errors occurred in req body", { errorGroup }))
+            next(createHttpError(400, "Some errors occurred in req body", {errorGroup}))
         }
-
     } catch (error) {
-        next()
+        next(error)
     }
 })
+
 
 
 export default mediaRouter
